182352b8c5a6af331149f12ce068156b7bd9cd44
rowerdog <2879672102@qq.com>
2021-09-03 10:13:41 +0800

    匿名函数

---
+++ src/main/scala/chapter06/Test05_Lambda.scala
@@ -0,0 +1,34 @@
+package main.scala.chapter06
+
+/**
+  * @ClassName: Test05_Lambda
+  * @Description
+  * @Author gengmb on 2021/9/3 9:15
+  * @Version: 1.0
+  */
+object Test05_Lambda {
+  def main(args: Array[String]): Unit = {
+
+    //匿名函数
+    val fun = (name: String) => println(name)
+    fun("bing")
+
+    //定义一个函数，函数的参数是一个函数
+
+    def f(func: String=>Unit): Unit = {
+      func("bing")
+    }
+    f(fun)
+
+    f((name: String)=>{println(name)})
+
+    f(name => println(name))
+
+    //如果参数只出现一次，则参数可以省略，且后面参数可以用_代替
+    f(println(_))
+
+    //函数的参数是一种类型的操作
+    f(println)
+
+  }
+}
